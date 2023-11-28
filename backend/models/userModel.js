10facb03f1c08d91b350645c46bf388fb784f8df
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-28 04:06:37 -0500

    Backend login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -43,10 +43,10 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
 });
 
-// // Decrypt password for authentication:
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//     return await bcrypt.compare(enteredPassword, this.password);
-// };
+// Decrypt password for authentication:
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Export userSchema as User:
 const User = mongoose.model("User", userSchema);
