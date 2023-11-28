ce19ff51be75a3dbea2c9f4586e8b30c6fb62241
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-28 03:31:34 -0500

    Backend User routes setup

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,22 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 const notes = require("./data/notes");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json()); // convert to json format
 
 app.get("/", (req, res) => {
     res.send("Note Taking Categorize Library API is listening of PORT 5000");
 });
+
 app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
-app.get("/api/notes/:id", (req, res) => {
-    const note = notes.find((note) => note.id === req.params.id);
-    res.send(note);
-});
 
-const PORT = process.env.PORT || 8000;
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server listening on PORT ${PORT}...`.blue));
